361f0916d70b9a949379335a53ff1e45ffcbecd4
Graevka <suvorovgraevka@gmail.com>
2025-11-22 19:59:52 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+RMA-Kernel Package
+Recursive Metacognitive Alignment for Large Language Models.
+"""
+
+from .supervisor import SupervisorAgent, CharterRule
+
+__version__ = "0.2.0-alpha"
+__author__ = "Graevka Lab"
+__license__ = "MIT"
+
+__all__ = ["SupervisorAgent", "CharterRule"]
