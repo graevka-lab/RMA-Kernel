62631578a6b061df0be088f92066697cb816f8b7
Graevka <suvorovgraevka@gmail.com>
2025-12-09 19:25:33 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,20 @@ RMA-Kernel Package
 Recursive Metacognitive Alignment for Large Language Models.
 """
 
-from .supervisor import SupervisorAgent, CharterRule
+from .supervisor import Supervisor
+from .verifier import Verifier
+from .generator import OpenAIGenerator, DummyGenerator
+from .embedding import STEmbeddingProvider, DummyEmbeddingProvider
 
-__version__ = "0.2.0-alpha"
+__version__ = "1.0.0-alpha"
 __author__ = "Graevka Lab"
-__license__ = "MIT"
+__license__ = "AGPL-3.0"
 
-__all__ = ["SupervisorAgent", "CharterRule"]
+__all__ = [
+    "Supervisor",
+    "Verifier",
+    "OpenAIGenerator",
+    "DummyGenerator",
+    "STEmbeddingProvider",
+    "DummyEmbeddingProvider"
+]
